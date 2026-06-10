45c69dffc90dadebb8984d2052ac0bfcd38653b0
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-06-11 00:41:18 +0300

    Db yapısı oluşturuldu, gerekli kütüphaneler kuruldu

---
+++ NotikaIdentityEmail/Context/EmailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotikaIdentityEmail.Context
+{
+    public class EmailContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-213H3J8\\SQLEXPRESS;Initial Catalog=EmailNotikaDb;Integrated Security=True;TrustServerCertificate=True;");
+        }
+    }
+}
